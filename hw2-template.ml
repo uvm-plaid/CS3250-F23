d3fa54673a6b2b23ffa292263b3aeb60410ec772
wintersmind <wintersmind@gmail.com>
2023-10-13 09:39:39 -0400

    Create hw2-template.ml

---
+++ hw2-template.ml
@@ -0,0 +1,71 @@
+(* CS3250 Programming Languages F23: Homework 2 *)
+
+(* Template, by ceskalka *)
+
+(*
+   NOTE: you should complete all parts where it says COMPLETE ME. Curently most are these
+   are functions with dummy bodies that are not correct solutions.
+*)
+
+(* Problem 1 *)
+
+(* 
+   mult : nat -> nat -> nat
+   in : pair (x, y) where x denotes a number n1 and y denotes n2
+   out : z that denotes n1 * n2
+ *)
+
+let rec mult n1 n2 = n2 (* COMPLETE ME *)
+
+(* Problem 2 *)
+
+(* 
+   Please include your answer here. A few sentences will suffice, but say as much as you 
+   like. COMPLETE ME 
+ *)
+
+(* Problem 3 *)
+
+(* 
+   member : 'a -> 'a list -> bool
+   in  : Value v, list l
+   out : True iff v is in l
+*)
+let rec member v l = false (* COMPLETE ME *)
+
+(* Problem 4 *)
+
+(* measurement datatype *)
+type emeasures = Meter of float | Liter of float | Centigrade of float
+type ameasures = Feet of float | Gallon of float | Fahrenheit of float
+
+(*
+  conversion : emeasures -> ameasures
+  in : english measurement x
+  out : conversion of x into corresponding american
+        measurement
+*)
+let conversion em = (Feet 0.0)  (* COMPLETE ME *)
+
+(* Problem 5 *)
+
+(* tree datatype *)
+type 'a tree = Leaf | Node of 'a tree * 'a * 'a tree
+
+(*
+  lookup : ('a * 'a -> bool) -> 'a -> 'a tree -> bool
+  in : total order lt, element x, tree t possessing 
+       BST property
+  out : true iff x is in t
+*)
+let rec lookup lt x t = false (* COMPLETE ME *)
+	
+(*
+  insert : ('a * 'a -> bool) -> 'a -> 'a tree -> 'a tree
+  in : total order lt, element x, tree t possessing 
+       BST property
+  out : tree t' which is t with x inserted such that t' 
+        possesses BST property 
+*)
+let rec insert lt x t = Leaf   (* COMPLETE ME *)
+
